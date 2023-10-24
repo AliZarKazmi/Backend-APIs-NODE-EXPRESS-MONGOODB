cc539ae58bc72db2b898d6b49de1b870f0e1fd49
AliZarKazmi <alizarkazmi55@gmail.com>
2023-10-24 01:14:11 -0700

    sorting not done yet

--- Store-Filtering API/controllers/products.js
+++ Store-Filtering API/controllers/products.js
@@ -1,10 +1,55 @@
+const Product = require('../models/product')
+
+//Filter functionality - finding specific data on basis of Name, Featured, Company
+//Method : HardCoded Filter
+
 const getAllProductStatic = async (req,res)=>{
-    res.status(200).json({msg:`products testing route`})
+  
+    const product = await Product.find({
+        // featured : true                  //finding specific data 
+    }).sort("name") // sort the "name" on asscending order 
+      
+    // sort the "name" on descending order and aslo sort "price" on asscending order
+    //.sort("-name price")
+
+    res.status(200).json({product, nbHits:product.length})
 }
 
+//Filter functionality - finding specific data on basis of Name, Featured, Company
+// Method: Dynamic Filter 
+//(support the concept of Query String)
 const getAllProduct = async (req,res)=>{
-    res.status(200).json({msg:`products  route`})
+    const {featured,company,name, sortData} = req.query
+    const queryObject = {}
+    if(featured){
+        queryObject.featured = featured==="true"?true:false
+    }
+    if(company){
+        queryObject.company= company
+    }
+
+    if(name){
+        queryObject.name={$regex: name , $options:'i'} // case insensitive search
+    }
+    console.log(queryObject)
+
+    //sorting logic
+    let result =  Product.find(queryObject)
+    if(sortData){
+        const sortList = sortData.split(',').join(' ')
+        result = result.sort(sortList)
+    }
+    else{
+        result = result.sort('createdAt')
+    }
+    const products = await result
+
+    res.status(200).json({product, nbHits:product.length})
 }
+
+//Diff b/t Params & Query String
+//Query String : For option data we prefer QueryString like "in filter functionality"
+//Params: Necessary data is share by params like "Id" 
 
 module.exports = {
     getAllProductStatic,
--- Store-Filtering API/populate.js
+++ Store-Filtering API/populate.js
@@ -1,3 +1,11 @@
+//in this file, we learn how to add json data file to
+// the db without entering manually
+
+//In short add Data to the database dynamically
+
+
+require ('dotenv').config()
+
 const connectDB= require("./db/connect")
 const Product = require("./models/product")
 
@@ -6,9 +14,13 @@ const jsonProducts = require("./products.json")
 const start = async () =>{
     try {
         await connectDB(process.env.MONGO_URI)
+        await Product.deleteMany()
+        await Product.create(jsonProducts)
         console.log("Success!!")
+        process.exit(0)
     } catch (error) {
         console.log(error)
+        process.exit(1)
     }
 }
 
