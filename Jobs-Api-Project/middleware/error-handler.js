414471fa10aa03a352dc89a5fd3edf89f9ab8238
AliZarKazmi <alizarkazmi55@gmail.com>
2023-12-08 19:35:25 +0500

    security info and packages

--- Jobs-Api-Project/middleware/error-handler.js
+++ Jobs-Api-Project/middleware/error-handler.js
@@ -1,10 +1,39 @@
-const { CustomAPIError } = require('../errors')
 const { StatusCodes } = require('http-status-codes')
 const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
+
+  let customError = {
+    //set default
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg : err.message || 'Something went wrong try again later'
   }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+  
+  //handling Mongoose error
+
+  //Validation Error
+  if(err.name === "ValidationError")
+  {
+    // console.log(Object.values(err.errors))
+    customError.msg = Object.values(err.errors).map((items)=>{items.message}).join(',')
+    customError.statusCode = 400
+  }
+
+  //Duplicate Error i.e if something like same email occur in the db  
+  if(err.code && err.code === 11000)
+  {
+   customError.msg = `Duplicate value entered for ${Object.keys(err.keyvalue)} field, please
+   choose another value`
+   customError.statusCode = 400  
+  }
+
+  //Cast Error i.e if there is any issue with the id (wrong _id)
+  if(err.name==='CastError')
+  {
+    customError.msg = `No item found with id :${err.value}`
+    customError.statusCode = 404
+  }
+
+  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+  return res.status(customError.statusCode).json({ msg : customError.msg })
 }
 
 module.exports = errorHandlerMiddleware
